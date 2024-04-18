49370c9c46753d4822489b37ddf4eac3ea1ec591
Samuel Mensah <samuelmensqh@gmail.com>
2024-04-18 11:45:30 +0000

    Code clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import './App.css'
+import "./App.css";
 import { Loader } from "@googlemaps/js-api-loader";
 import { useEffect, useRef, useState } from "react";
-import geoData from "../data.json"
-import Pharmacy from '../src/pharmacy_plus.png'
-import { ChevronDownCircle } from "lucide-react";
+import geoData from "../data.json";
+import Pharmacy from "../src/pharmacy_plus.png";
+import { ChevronDownCircle, HospitalIcon, LinkIcon, MailIcon, MapPinIcon, PhoneIcon } from "lucide-react";
 
-// const pharmaImg = 
+// const pharmaImg =
 
 function App() {
-
   const mapRef = useRef<HTMLDivElement>(null);
-  const [infoWindowContent, setInfoWindowContent] = useState<string | null>(null)
-  const [infoWindowPosition, setInfoWindowPosition] = useState<{ lat: number, lng: number} | null>(null)
+  const [pharma, setPharma] = useState("SELECT PHARMACY");
 
-  const data = geoData.features.map((item)=> item.properties);
+  const data = geoData.features.map((item) => item.properties);
 
   useEffect(() => {
     const initMap = async () => {
@@ -22,102 +20,131 @@ function App() {
         version: "weekly",
       });
 
-      const { Map } = await loader.importLibrary('maps');
-      const { Marker } = await loader.importLibrary('marker');
+      const { Map } = await loader.importLibrary("maps");
+      const { Marker } = await loader.importLibrary("marker");
 
       // Get current Location using Geolocation API
-      navigator.geolocation.getCurrentPosition((position) => {
-        const currentPosition = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const currentPosition = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
 
+          const mapOptions = {
+            center: currentPosition,
+            zoom: 14,
+            mapId: "pharma_plus",
+          };
 
-        const mapOptions = {
-          center: currentPosition,
-          zoom: 14,
-          mapId: 'pharma_plus',
-        };
+          // Custom marker for pharmacies
+          const icon = {
+            url: Pharmacy,
+            scaledSize: new google.maps.Size(30, 30),
+          };
 
-        // Custom marker for pharmacies
-        const icon = {
-          url: Pharmacy,
-          scaledSize: new google.maps.Size(30, 30)
-        }
+          // Setting up Map;
+          const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
 
-        // Setting up Map;
-        const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
-  
-        // The marker, positioned at Current Location
-        new Marker({
-          map: map,
-          position: currentPosition,
-          title: 'Current Location'
-        });
+          // The marker, positioned at Current Location
+          new Marker({
+            map: map,
+            position: currentPosition,
+            title: "Current Location",
+          });
 
-        // Loading GeoJson data
+          // Loading GeoJson data
           map.data.addGeoJson(geoData, {
-            idPropertyName: 'name'
+            idPropertyName: "name",
           });
 
           map.data.setStyle((geoData: any) => {
-            return {
+            geoData = {
               icon: icon
-            };
+            }
+            return geoData;
           });
-
-          // Attaching click event listener to each marker
-          map.data.addListener('click', (event:any) => {
-            const markerPosition = event.feature.getGeometry().get();
-            const markerInfo = event.feature.getProperty('name');
-            
-            // Set InfoWindow content and position
-            setInfoWindowContent(`<div><h3>${markerInfo}</h3><p>${markerInfo.description}</p></div>`);
-            setInfoWindowPosition(markerPosition);
-          })
-        
-      }, (error) => {
-        console.error("Error getting current location", error)
-      });
+        },
+        (error) => {
+          console.error("Error getting current location", error);
+        }
+      );
     };
-
 
     initMap();
   }, []);
 
   return (
     <>
-      <div style={{height: '100vh', width: '100vw', position: 'relative'}} ref={mapRef}>
-      </div>
+      <div
+        style={{ height: "100vh", width: "100vw", position: "relative" }}
+        ref={mapRef}
+      ></div>
       <div className="card">
-
-      {/* Profile and select box for hospital */}
-      <div className='prf'>
-        <div className='profile-img-container'>
-          <img src={Pharmacy} alt="profile image" className="profile-img"/>
-          <ChevronDownCircle width={20} height={20} color='#c94277'/>
-        </div>
-        <div>
-          <form action="" method="get">
-            <select name="">
-              <option value="">SELECT PHARMACY</option>
-                {data.map((i)=> (
-                  <option value="" key={i.email}>
-                    {i.name}
+        {/* Profile and select box for hospital */}
+        <div className="prf">
+          <div className="profile-img-container">
+            <img src={Pharmacy} alt="profile image" className="profile-img" />
+            <ChevronDownCircle width={20} height={20} color="#c94277" />
+          </div>
+          <div>
+            <form action="" method="get">
+              <select
+                value={pharma}
+                onChange={(e) => setPharma(e.target.value)}
+              >
+                <option value="">SELECT PHARMACY</option>
+                {data.map((i) => (
+                  <option value={i.name} key={i.email}>
+                    {i.name.toUpperCase()}
                   </option>
                 ))}
-            </select>
-          </form>
+              </select>
+            </form>
+          </div>
         </div>
-      </div>
-      {/* Profile and select box for hospital */}
+        {/* Profile and select box for hospital */}
 
+        {/* Selected Pharmacy information*/}
+        {pharma &&
+          data.map((item) => {
+            if (item.name == pharma) {
+              return (
+                <div className="hosp-container">
+                  <div className="hosp-info">
+                    <div>
+                      <HospitalIcon color="#c94277"/>
+                    </div>
+                    <div>
+                      <p>{item.name}</p>
+                    </div>
+                  </div>
+
+                  <div className="hosp-info">
+                    <MapPinIcon color="#c94277"/>
+                    <p>{item.address}</p>
+                  </div>
+
+                  <div className="hosp-info">
+                    <MailIcon color="#c94277"/>
+                    <p>{item.email}</p>
+                  </div>
+
+                  <div className="hosp-info">
+                    <LinkIcon color="#c94277"/>
+                    <p>{item.website}</p>
+                  </div>
+
+                  <div className="hosp-info">
+                    <PhoneIcon color="#c94277"/>
+                    <p>{item.Tel}</p>
+                  </div>
+                </div>
+              );
+            }
+          })}
+        {/* Selected Pharmacy information*/}
       </div>
-      {/* {infoWindowContent && infoWindowPosition && (
-        <div style={{ position: 'absolute', top: 50, left: 20, backgroundColor: 'white', padding: 10, borderRadius: 5 }}>
-          <div dangerouslySetInnerHTML={{ __html: infoWindowContent }}></div>
-        </div>
-      )} */}
     </>
   );
 }
